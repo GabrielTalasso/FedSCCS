861506913d0347a7a024c4f5908ed662911d8670
GabrielTalasso <gukstin@gmail.com>
2023-06-21 17:09:43 -0300

    new file:   clustering_fl/data/25/idx_test_0.pickle 	new file:   clustering_fl/data/25/idx_test_1.pickle 	new file:   clustering_fl/data/25/idx_test_10.pickle 	new file:   clustering_fl/data/25/idx_test_11.pickle 	new file:   clustering_fl/data/25/idx_test_12.pickle 	new file:   clustering_fl/data/25/idx_test_13.pickle 	new file:   clustering_fl/data/25/idx_test_14.pickle 	new file:   clustering_fl/data/25/idx_test_15.pickle 	new file:   clustering_fl/data/25/idx_test_16.pickle 	new file:   clustering_fl/data/25/idx_test_17.pickle 	new file:   clustering_fl/data/25/idx_test_18.pickle 	new file:   clustering_fl/data/25/idx_test_19.pickle 	new file:   clustering_fl/data/25/idx_test_2.pickle 	new file:   clustering_fl/data/25/idx_test_20.pickle 	new file:   clustering_fl/data/25/idx_test_21.pickle 	new file:   clustering_fl/data/25/idx_test_22.pickle 	new file:   clustering_fl/data/25/idx_test_23.pickle 	new file:   clustering_fl/data/25/idx_test_24.pickle 	new file:   clustering_fl/data/25/idx_test_3.pickle 	new file:   clustering_fl/data/25/idx_test_4.pickle 	new file:   clustering_fl/data/25/idx_test_5.pickle 	new file:   clustering_fl/data/25/idx_test_6.pickle 	new file:   clustering_fl/data/25/idx_test_7.pickle 	new file:   clustering_fl/data/25/idx_test_8.pickle 	new file:   clustering_fl/data/25/idx_test_9.pickle 	new file:   clustering_fl/data/25/idx_train_0.pickle 	new file:   clustering_fl/data/25/idx_train_1.pickle 	new file:   clustering_fl/data/25/idx_train_10.pickle 	new file:   clustering_fl/data/25/idx_train_11.pickle 	new file:   clustering_fl/data/25/idx_train_12.pickle 	new file:   clustering_fl/data/25/idx_train_13.pickle 	new file:   clustering_fl/data/25/idx_train_14.pickle 	new file:   clustering_fl/data/25/idx_train_15.pickle 	new file:   clustering_fl/data/25/idx_train_16.pickle 	new file:   clustering_fl/data/25/idx_train_17.pickle 	new file:   clustering_fl/data/25/idx_train_18.pickle 	new file:   clustering_fl/data/25/idx_train_19.pickle 	new file:   clustering_fl/data/25/idx_train_2.pickle 	new file:   clustering_fl/data/25/idx_train_20.pickle 	new file:   clustering_fl/data/25/idx_train_21.pickle 	new file:   clustering_fl/data/25/idx_train_22.pickle 	new file:   clustering_fl/data/25/idx_train_23.pickle 	new file:   clustering_fl/data/25/idx_train_24.pickle 	new file:   clustering_fl/data/25/idx_train_3.pickle 	new file:   clustering_fl/data/25/idx_train_4.pickle 	new file:   clustering_fl/data/25/idx_train_5.pickle 	new file:   clustering_fl/data/25/idx_train_6.pickle 	new file:   clustering_fl/data/25/idx_train_7.pickle 	new file:   clustering_fl/data/25/idx_train_8.pickle 	new file:   clustering_fl/data/25/idx_train_9.pickle 	modified:   clustering_fl/tests.py

--- clustering_fl/tests.py
+++ clustering_fl/tests.py
@@ -7,12 +7,12 @@ import pandas as pd
 
 
 n_clients = 7
-data_path = 'data/MNIST'
+data_path = 'data/25'
 
-with open(f'./{data_path}/0/classes_per_client_10/alpha_0.1/idx_train_0.pickle', 'rb') as file:
+with open(f'./data/25/idx_test_0.pickle', 'rb') as file:
     f  = pickle.load(file)
 #data/MNIST/0/classes_per_client_10/alpha_0.1/idx_test_0.pickle
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 #
-#print(f)
+print(f)
 x_train[f]
