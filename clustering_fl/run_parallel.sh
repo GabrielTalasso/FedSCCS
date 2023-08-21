89d7f7cf9c968d076542545bbbcb14e6f5a9f031
GabrielTalasso <gukstin@gmail.com>
2023-08-21 13:17:54 -0300

    modified:   clustering_fl/run_parallel.sh

--- clustering_fl/run_parallel.sh
+++ clustering_fl/run_parallel.sh
@@ -24,7 +24,7 @@ METRICLAYER="-1 -2 1"
 
 POCPERCOFCLIENTS=0.5
 
-CLUSTERMETHOD="KCenter Affinity HC Random"
+CLUSTERMETHOD="Affinity"
 
 parallel --bar -j $CORES "python3 simulation.py --dataset {1} --nclients {2} \
  --nrounds {3} --nclusters {4} --clustering {5} --clusterround {6} --noniid {7} \
