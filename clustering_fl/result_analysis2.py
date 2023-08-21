92cf316515319e833dcc471d2b0d466340efa278
GabrielTalasso <gukstin@gmail.com>
2023-08-21 14:46:03 -0300

    new file:   clustering_fl/analysis.ipynb 	modified:   clustering_fl/result_analysis2.py

--- clustering_fl/result_analysis2.py
+++ clustering_fl/result_analysis2.py
@@ -5,16 +5,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 n_clusters = 10
-selection = 'POC'
+selection = 'All'
 method = 'HC'
 
 
-paths = [f'local_logs/MotionSense/CKA-(-1)-{method}-{selection}-0.5/evaluate/acc_24clients_{n_clusters}clusters.csv',
-         f'local_logs/MotionSense/weights-(-1)-{method}-{selection}-0.5/evaluate/acc_24clients_{n_clusters}clusters.csv',
-         f'local_logs/MotionSense/weights-(-1)-HC-{selection}-0.5/evaluate/acc_24clients_1clusters.csv',
-         f'local_logs/MotionSense/weights-(-1)-KCenter-{selection}-0.5/evaluate/acc_24clients_{n_clusters}clusters.csv',
-         f'local_logs/MotionSense/CKA-(-1)-Random-{selection}-0.5/evaluate/acc_24clients_{n_clusters}clusters.csv']
+paths = [f'simulation1/local_logs/MotionSense/CKA-(-1)-{method}-{selection}-0.5/evaluate/acc_24clients_{n_clusters}clusters.csv',
+         f'simulation1/local_logs/MotionSense/weights-(-1)-{method}-{selection}-0.5/evaluate/acc_24clients_{n_clusters}clusters.csv',
+         f'simulation1/local_logs/MotionSense/weights-(-1)-HC-{selection}-0.5/evaluate/acc_24clients_1clusters.csv',
+         f'simulation1/local_logs/MotionSense/weights-(-1)-KCenter-{selection}-0.5/evaluate/acc_24clients_{n_clusters}clusters.csv',
+         f'simulation1/local_logs/MotionSense/CKA-(-1)-Random-{selection}-0.5/evaluate/acc_24clients_{n_clusters}clusters.csv']
 
 for p in paths:
     print(p)
@@ -39,3 +40,5 @@ for p in paths:
 
     sns.lineplot(acc.groupby('rounds').mean(), y = 'acc', x = 'rounds', legend='brief', label=label_clusters)
 plt.show()
+
+
